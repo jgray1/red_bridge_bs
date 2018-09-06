ba42406faf8f781c795b082cd017bdf4e244c4dc
jgray1 <john.edwin.gray@gmail.com>
2018-09-07 10:53:52 +1200

    Additional HTML and styling. Add js to scroll to anchors.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+baguetteBox.run('.gallery', {
+  buttons: 'auto'
+});
+
+$(function() {
+  var topoffset = 70; //variable for menu height
+
+  //Use smooth scrolling when clicking on navigation
+  //Source: https://gist.github.com/WebDevBooster/5fb2805fa19f4f9ae0d6bc9e3073e3e9
+  $('.navbar-nav a').click(function() {
+    if (location.pathname.replace(/^\//,'') ===
+      this.pathname.replace(/^\//,'') &&
+      location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
+
+});
