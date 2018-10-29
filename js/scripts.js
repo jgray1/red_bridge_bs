7d088063e605ef5ec9b48703c582f3e9d8be3195
jgray1 <john.edwin.gray@gmail.com>
2018-10-29 15:47:44 +1300

    Replace bagette box image gallery with magnific. Clean up HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,31 @@
-baguetteBox.run('.gallery', {
-  buttons: 'auto'
-});
+$(document).ready(function() {
 
-//Source: https://gist.github.com/WebDevBooster/5fb2805fa19f4f9ae0d6bc9e3073e3e9
-$(function() {
-  var topoffset = 70; //variable for menu height
-  //Use smooth scrolling when clicking on navigation
-  $('.navbar-nav a').click(function() {
-    if (location.pathname.replace(/^\//,'') ===
-      this.pathname.replace(/^\//,'') &&
-      location.hostname === this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top-topoffset
-        }, 500);
-        return false;
-      } //target.length
-    } //click function
-  }); //smooth scrolling
+  $('.lightbox').magnificPopup({
+    type: 'image',
+    gallery:{
+      enabled:true
+    }
+  });
+
+  //Source: https://gist.github.com/WebDevBooster/5fb2805fa19f4f9ae0d6bc9e3073e3e9
+  $(function() {
+    var topoffset = 70; //variable for menu height
+    //Use smooth scrolling when clicking on navigation
+    $('.navbar-nav a').click(function() {
+      if (location.pathname.replace(/^\//,'') ===
+        this.pathname.replace(/^\//,'') &&
+        location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top-topoffset
+          }, 500);
+          return false;
+        } //target.length
+      } //click function
+    }); //smooth scrolling
+
+  });
 
 });
